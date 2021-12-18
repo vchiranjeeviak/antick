2d34aff01f5fb55b18d719c23ac6c2ab9ab16147
kotapatianuhya <68712844+kotapatianuhya@users.noreply.github.com>
2021-12-18 23:30:24 +0530

    added deleteuser functionality

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { getUserById, getUser,modifyUser } = require('../controllers/user');
+const { getUserById, getUser,modifyUser,deleteUser } = require('../controllers/user');
 const { isSignedIn,isAuthenticated } = require('../controllers/auth');
 
 router.param('userId', getUserById);
 
 router.get('/user/:userId', isSignedIn, getUser);
 router.put('/user/:userId',isSignedIn,isAuthenticated,modifyUser);
+router.delete('/user/:userId',isSignedIn,isAuthenticated,deleteUser);
 
 module.exports = router;
